83b773dd8d5bedae340e202e4610c9f2b937bfd0
Raul Florez <raul.florez@sqasa.co>
2024-05-01 22:09:54 -0500

    commit3 rama2 para el reto

---
+++ src/main/java/com/defaultpackage/question/DefaultQuestion3.java
@@ -0,0 +1,14 @@
+package com.defaultpackage.question;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+
+public class DefaultQuestion3 implements Question <String> {
+
+    private static String test3;
+
+    @Override
+    public String answeredBy(Actor actor) {
+        return null;
+    }
+}
